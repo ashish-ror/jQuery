57c411585064092f13fc210b4647be7de3d02710
tanmay3011 <tanmay3011@gmail.com>
2014-06-27 20:35:03 +0530

    Commit after blur event functionality correction

--- Exercise4/inputHint.js
+++ Exercise4/inputHint.js
@@ -1,39 +1,25 @@
 /*Input hint on search inputbox and add remove class on the basis of event*/
 $(function() {
-  var labelText = $('label[for = "q"]').text();
+  var labeledText = $('label[for = "q"]').text();
   
   // Set the value of the search input to the text of the label element
   // Add a class of "hint" to the search input
-  $('.input_text').val(labelText).addClass("hint");
+  $('.input_text').val(labeledText).addClass("hint");
   
   // Remove the label element
   $('label[for = "q"]').remove();
-  
   $('.input_text').on({
     // Bind a focus event to the search input that removes the hint text and the "hint" class
     focus: function () {
-      if ($(this).val().trim() == labelText) {
+      if ($(this).val().trim() == labeledText) {
         $(this).val("").removeClass("hint");
       }
     },
     // Bind a blur event to the search input that restores the hint text and "hint" class if no search text was entered
     blur: function () {
       if (!$(this).val().trim()) {
-        $(this).val(labelText).addClass("hint");
+        $(this).val(labeledText).addClass("hint");
       }
     }
   });
 });
-
-
-
-
-
-
-  
-
-  
-
-  
-
-    
