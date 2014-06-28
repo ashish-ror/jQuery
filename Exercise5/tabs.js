a2a580a9430719badffb54a7f357d72236f4d87f
tanmay3011@gmail.com <tanmay3011@gmail.com>
2014-06-28 15:43:28 +0530

    Initial commit after Exercise5

--- Exercise5/tabs.js
+++ Exercise5/tabs.js
@@ -1,37 +1,30 @@
-/*Input hint on search inputbox and add remove class on the basis of event*/
+/*Tabbed Navigation*/
 $(function() {
-  var labelText = $('label[for = "q"]').text();
   
-  // Set the value of the search input to the text of the label element
-  // Add a class of "hint" to the search input
-  $('.input_text').val(labelText).addClass("hint");
+  // Q.1) Hide all of the modules.
+  var $modules = $('.module').hide();
   
-  // Remove the label element
-  $('label[for = "q"]').remove();
-  
-  $('.input_text').on({
-    // Bind a focus event to the search input that removes the hint text and the "hint" class
-    focus: function () {
-        $(this).val("").removeClass("hint");
-    },
-    // Bind a blur event to the search input that restores the hint text and "hint" class if no search text was entered
-    blur: function () {
-      if (!$(this).val().trim()) {
-        $(this).val(labelText).addClass("hint");
-      }
-    }
-  });
-});
-
-
-
-
-
-
-  
-
-  
-
-  
-
+  // // Q.2) Create an unordered list element before the first module.
+  var $unorderedListElement = $("<ul>").insertBefore($modules.eq(0));
     
+  // Q.3) Iterate over the modules using $.fn.each. For each module, use the text of the h2 element as the text for a list item that you add to the unordered list element.
+  $modules.each(function() {
+    var $that = $(this),
+      headerText = $(this).find('h2').text(),
+      $listElement = $('<li>').text(headerText).appendTo($unorderedListElement);
+
+    //Q.4) Bind a click event to the list item that:
+    $listElement.click(function() {
+      // Shows the related module, and hides any other modules
+      $that.show().siblings('.module').hide();
+
+      // Adds a class of "current" to the clicked list item
+      // Removes the class "current" from the other list item
+      $listElement.addClass('current').siblings().removeClass('current');
+    });
+  });
+
+  // Finally, show the first tab.
+  $modules.eq(0).show();
+  $unorderedListElement.find('li').eq(0).addClass('current');
+});
