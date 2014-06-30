c3e729c39621af57f3388d25eca4b8ea7f2df844
tanmay3011 <tanmay3011@gmail.com>
2014-06-30 11:26:33 +0530

    Commit after DOM hit reduction and event binding out of loop

--- Exercise5/tabs.js
+++ Exercise5/tabs.js
@@ -1,31 +1,33 @@
 /*Tabbed Navigation*/
-$(function() {
+$(document).ready(function() {
   
   // Q.1) Hide all of the modules.
   var $modules = $('.module').hide();
   
-
   // // Q.2) Create an unordered list element before the first module.
-  var $unorderedListElement = $("<ul>").insertBefore($modules.eq(0));
+  var $unorderedListElement = $('<ul>').insertBefore($modules.eq(0));
     
   // Q.3) Iterate over the modules using $.fn.each. For each module, use the text of the h2 element as the text for a list item that you add to the unordered list element.
-  var $listElement;
-  $('.module h2').each(function() {
+  var $modulesHeaderElement = $modules.find('h2');
+  $modulesHeaderElement.each(function() {
     var headerText = $(this).text();
     $('<li>').text(headerText).appendTo($unorderedListElement);
   });
 
+  var $modulesListElement = $unorderedListElement.find('li');
+  
   //Q.4) Bind a click event to the list item that:
-  $('.module ul li').click(function() {
-      // Shows the related module, and hides any other modules
-    $modules.show().siblings('.module').hide();
+  $modulesListElement.click(function() {
+    var $that = $(this);
+    // Shows the related module, and hides any other modules
+    var elementId = "#" + $that.text().toLowerCase();
+    $(elementId).show().siblings('.module').hide();
 
-      // Adds a class of "current" to the clicked list item
-      // Removes the class "current" from the other list item
-    $(this).addClass('current').siblings().removeClass('current');
+    // Adds a class of "current" to the clicked list item
+    // Removes the class "current" from the other list item
+    $that.addClass('current').siblings().removeClass('current');
   });
 
   // Finally, show the first tab.
   $modules.eq(0).show();
-  //$unorderedListElement.find('li').eq(0).addClass('current');
 });
