35fcd876d2628ee1a6505302ffff672e4aa2109a
tanmay3011 <tanmay3011@gmail.com>
2014-06-30 11:32:05 +0530

    Commit after DOM hit reduction and document ready addition

--- Exercise6/blog.js
+++ Exercise6/blog.js
@@ -1,11 +1,12 @@
 /*Reveal Hidden Text*/
-$(function() {
+$(document).ready(function() {
   var $blogHeadingElements = $('#blog ul li');
   $blogHeadingElements.click(function(event) {
+  	var $blogHeading = $(this);
     event.preventDefault();
     //Clicking on a headline in the #blog div should slide down the excerpt paragraph
-    $(this).find('.excerpt').slideDown();
+    $blogHeading.find('.excerpt').slideDown();
     //Clicking on another headline should slide down its excerpt paragraph, and slide up any other currently showing excerpt paragraphs.
-    $(this).siblings('li').find('.excerpt:visible').slideUp();
+    $blogHeading.siblings('li').find('.excerpt:visible').slideUp();
   });
 });
