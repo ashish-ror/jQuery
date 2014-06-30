11ec231ab97ce3259ecd2027f56fea814516de2b
tanmay3011 <tanmay3011@gmail.com>
2014-06-30 19:38:19 +0530

    Initial commit after Exercise9- Load External Content

---
+++ Exercise9/load.js
@@ -0,0 +1,25 @@
+/*Load External Content*/
+$(document).ready(function () {
+  var $targetURL = '';
+  //Create a target div after the headline for each blog post
+  //store a reference to it on the headline element using $.fn.data.
+  var $blogHeadingElement = $('#blog h3');
+  $blogHeadingElement.each(function (index) {
+    var $blogHeading = $(this),
+      $targetDiv = $('<div>').insertAfter($blogHeading);
+    $blogHeading.data('reference', $targetDiv);
+  })
+  //Bind a click event to the headline that will use the $.fn.load
+  //method to load the appropriate content from /exercises/data/blog.html into the target div.
+  $blogHeadingElement.click(function (event) {
+    var $blogHeading = $(this);
+    event.preventDefault();
+    $targetURL = $blogHeading.children('a').attr('href');
+    $targetURL = 'data/' + $targetURL.replace('#', " #");
+    $blogHeading.data('reference').load($targetURL);
+  });
+});
+
+
+
+
