d1f9cd3b237c28e2c774a8dbbaa289a3190fc9d8
tanmay3011 <tanmay3011@gmail.com>
2014-07-02 15:25:40 +0530

    Initial Commit Exercise11- Div of Stacks

---
+++ Exercise11/divsStack.js
@@ -0,0 +1,44 @@
+var Stack = function ($addButton, $containerElement) {
+  this.addButton = $addButton;
+  this.containerElement = $containerElement;
+};
+
+Stack.prototype = {
+  //method to bind create and add event on add button click
+  bindEvents : function () {
+    var that = this;
+    this.addButton.click(function() {
+      that.createAndAddDiv();
+    });
+    this.containerElement.on('click', 'div', function() {
+      var $divElement = $(this);
+      that.highlightDiv($divElement);
+      that.removeDiv($divElement);
+    });
+  },
+
+  //method to create a new div and then append it to the main container
+  createAndAddDiv : function () {
+    this.divCount = this.containerElement.children().length + 1;
+    var $divElement = $("<div>").addClass('stackElement').text(this.divCount);
+    $divElement.prependTo(this.containerElement);
+  },
+
+  //method to higlight div 
+  highlightDiv : function ($divElement) {
+      $divElement.toggleClass('highlight');
+  },
+  
+  //method to remove topmost div if clicked
+  removeDiv : function ($divElement) {
+    if(this.divCount == $divElement.text()) {
+      $divElement.remove();
+      this.divCount -= 1;
+    }
+  }
+}
+
+$(document).ready(function () {
+  stack = new Stack($("#add"), $("#container"));
+  stack.bindEvents();
+})
