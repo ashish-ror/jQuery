34920eedc02bf63f98be0e57e35060026184b130
tanmay3011 <tanmay3011@gmail.com>
2014-07-04 21:14:05 +0530

    Name Correction addEelementToStack to addElement

--- Exercise11/divsStack.js
+++ Exercise11/divsStack.js
@@ -3,12 +3,11 @@ var Stack = function ($addButton, $containerElement) {
   this.addButton = $addButton;
   this.containerElement = $containerElement;
   this.elementCount = 0;
-  this.lastElementCount = 0;
 };
 
 var Element = function (elementCount) {
   this.elementCount = elementCount;
-  this.element = $("<div>").addClass('stackElement').text(this.elementCount);
+  this.element = $('<div>').addClass('stackElement').text(this.elementCount);
 };
 
 Stack.prototype = {
@@ -17,11 +16,12 @@ Stack.prototype = {
     var _this = this;
     this.addButton.click(function () {
       _this.elementCount++;
-      _this.addElementToStack(new Element(_this.elementCount).element);
+      _this.addElement(new Element(_this.elementCount).element);
     });
+
     this.containerElement.on('click', '.stackElement', function () {
       var $element = $(this);
-      if ($element.is(":first-child")) {
+      if ($element.is(':first-child')) {
         _this.removeElement($element);
       } else {
         _this.highlightElement($element);
@@ -30,7 +30,7 @@ Stack.prototype = {
   },
 
   //method to create a new div and then append it to the main container
-  addElementToStack : function ($newElement) {
+  addElement : function ($newElement) {
     $newElement.prependTo(this.containerElement);
   },
 
@@ -46,6 +46,5 @@ Stack.prototype = {
 };
 
 $(function () {
-  "use strict";
-  new Stack($("#add"), $("#container")).bindEvents();
+  new Stack($('#add'), $('#container')).bindEvents();
 });
