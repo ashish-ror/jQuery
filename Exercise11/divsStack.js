d574cd244488ef5c068f459e093b445f55c63c67
tanmay3011 <tanmay3011@gmail.com>
2014-07-04 20:49:40 +0530

    Remove lastElement variable since last element can be extracted by using is() method

--- Exercise11/divsStack.js
+++ Exercise11/divsStack.js
@@ -1,6 +1,5 @@
 /*Stack of Divs*/
 var Stack = function ($addButton, $containerElement) {
-  "use strict";
   this.addButton = $addButton;
   this.containerElement = $containerElement;
   this.elementCount = 0;
@@ -8,32 +7,23 @@ var Stack = function ($addButton, $containerElement) {
 };
 
 var Element = function (elementCount) {
-  "use strict";
   this.elementCount = elementCount;
-};
-
-Element.prototype = {
-  create : function () {
-    "use strict";
-    this.element = $("<div>").addClass('stackElement').data("highlight", false).text(this.elementCount);
-    return this.element;
-  }
+  this.element = $("<div>").addClass('stackElement').text(this.elementCount);
 };
 
 Stack.prototype = {
   //method to bind create and add event on add button click
   bindEvents : function () {
-    "use strict";
     var _this = this;
     this.addButton.click(function () {
       _this.elementCount++;
-      _this.lastElementValue = _this.elementCount;
-      _this.addElementToStack(new Element(_this.elementCount).create());
+      _this.addElementToStack(new Element(_this.elementCount).element);
     });
     this.containerElement.on('click', '.stackElement', function () {
-      var $element = $(this),
-        isElementRemoved = _this.removeElement($element);
-      if (!isElementRemoved && !$element.data("highlight")) {
+      var $element = $(this);
+      if ($element.is(":first-child")) {  
+        _this.removeElement($element);
+      } else {
         _this.highlightElement($element);
       }
     });
@@ -41,25 +31,17 @@ Stack.prototype = {
 
   //method to create a new div and then append it to the main container
   addElementToStack : function ($newElement) {
-    "use strict";
     $newElement.prependTo(this.containerElement);
   },
 
   //method to higlight div
   highlightElement : function ($element) {
-    "use strict";
-    $element.addClass('highlight').data("highlight", true);
+    $element.addClass('highlight');
   },
 
   //method to remove topmost div if clicked
   removeElement : function ($element) {
-    "use strict";
-    if (this.lastElementValue == $element.text()) {
-      $element.remove();
-      this.lastElementValue = this.containerElement.children().first().text();
-      return true;
-    }
-    return false;
+    $element.remove();
   }
 };
 
