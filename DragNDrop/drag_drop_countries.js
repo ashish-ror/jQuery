a491f1d1688dcab2a1eb02e0b6d010ebfcd82425
tanmay3011 <tanmay3011@gmail.com>
2014-07-07 19:59:15 +0530

    Define Individual prototype methods instead defining in one prototype

--- DragNDrop/drag_drop_countries.js
+++ DragNDrop/drag_drop_countries.js
@@ -4,35 +4,33 @@ var DragAndDropManager = function($countryList1, $countryList2) {
   this.countryList2 = $countryList2;
 };
 
-DragAndDropManager.prototype = {
-  bindEvents : function() {
-    "use strict";
-    var _this = this;
-    this.countryList1.on("mouseenter", "li", function() {
-      _this.dragAndDrop(_this.countryList1, _this.countryList2);
-    });
+DragAndDropManager.prototype.bindEvents = function() {
+  "use strict";
+  var _this = this;
+  this.countryList1.on("mouseenter", "li", function() {
+    _this.dragAndDrop(_this.countryList1, _this.countryList2);
+  });
 
-    this.countryList2.on("mouseenter", "li", function() {
-      _this.dragAndDrop(_this.countryList2, _this.countryList1);
-    });
-  },
+  this.countryList2.on("mouseenter", "li", function() {
+    _this.dragAndDrop(_this.countryList2, _this.countryList1);
+  });
+},
 
-  dragAndDrop : function($list1, $list2) {
-    "use strict";
-    $list1.find("li").draggable({
-      revert: "invalid",
-      helper: "clone",
-      cursor: "move"
-    }).disableSelection();
+DragAndDropManager.prototype.dragAndDrop = function($list1, $list2) {
+  "use strict";
+  $list1.find("li").draggable({
+    revert: "invalid",
+    helper: "clone",
+    cursor: "move"
+  }).disableSelection();
 
-    $list2.droppable({
-      accept: "li",
-      activeClass: "ui-state-highlight",
-      drop: function(event, ui) {
-        ui.draggable.removeAttr("style").appendTo($list2);
-      }
-    });
-  }
+  $list2.droppable({
+    accept: "li",
+    activeClass: "ui-state-highlight",
+    drop: function(event, ui) {
+      ui.draggable.removeAttr("style").appendTo($list2);
+    }
+  });
 };
 
 $(function($) {
